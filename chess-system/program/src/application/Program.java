52be4cbd86b41941a7cc7a484866a9b80491c1ed
caioo <matossousacaio.cms@gmail.com>
2024-08-13 16:53:53 -0300

    Classes Board e Piece
    Classes iniciais das peças do tabuleiro e a classe do tabuleiro em si

--- chess-system/program/src/application/Program.java
+++ chess-system/program/src/application/Program.java
@@ -1,8 +1,11 @@
 package application;
 
+import boardgame.Board;
+
 public class Program {
     public static void main(String[] args) throws Exception {
 
+        Board board = new Board(8, 8);
         
     }
 }
